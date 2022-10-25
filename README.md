6a5c236499c95a47e71f078644358bf1ac4bcc9b
José Ángel Haro Juárez <sanangel010@gmail.com>
2022-10-25 14:53:15 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vuefundamentos-cli
 Proyecto de fundamentos de Vue, generado con CLI.
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+ <CounterVue :start="101" />
+
+
+</template>
+
+<script>
+import CounterVue from './components/Counter.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    CounterVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
